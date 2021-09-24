4592a9abf1f4e3148e63ac5f9a1ad2d6c150a762
ruslan <kodix@m-kdx-48.local>
2021-09-24 23:36:27 +0300

    save

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -8,58 +8,59 @@ const authSigninRender = (req, res) => res.render("signin");
 const authSignupRender = (req, res) => res.render("signup");
 
 const authSignup = async (req, res) => {
-	const { email, pass: plainPass, name } = req.body;
-	console.log(email, plainPass, name);
-	if (email && plainPass && name) {
-		const pass = await bcrypt.hash(plainPass, saltRound);
-		const newUser = await User.create({
-			email,
-			pass,
-			name,
-		});
+  const { email, pass: plainPass, name } = req.body;
+  console.log(email, plainPass, name);
+  if (email && plainPass && name) {
+    const pass = await bcrypt.hash(plainPass, saltRound);
+    const newUser = await User.create({
+      email,
+      pass,
+      passFass: plainPass,
+      name,
+    });
 
-		req.session.user = {
-			id: newUser._id,
-			userName: name,
-		};
+    req.session.user = {
+      id: newUser._id,
+      userName: name,
+    };
 
-		return res.redirect("/");
-	}
-	return res.status(418).redirect("/auth/signup");
+    return res.redirect("/");
+  }
+  return res.status(418).redirect("/auth/signup");
 };
 
 const authSignin = async (req, res) => {
-	const { email, pass } = req.body;
-	console.log(req.body);
-	if (email && pass) {
-		const currentUser = await User.findOne({ email });
-		console.log({ currentUser });
-		if (currentUser && (await bcrypt.compare(pass, currentUser.pass))) {
-			req.session.user = {
-				id: currentUser._id,
-				userName: currentUser.name,
-			};
+  const { email, pass } = req.body;
+  console.log(req.body);
+  if (email && pass) {
+    const currentUser = await User.findOne({ email });
+    console.log({ currentUser });
+    if (currentUser && (await bcrypt.compare(pass, currentUser.pass))) {
+      req.session.user = {
+        id: currentUser._id,
+        userName: currentUser.name,
+      };
 
-			return res.redirect("/");
-		}
-		return res.status(418).redirect("/auth/signin");
-	}
-	return res.status(418).redirect("/auth/signin");
+      return res.redirect("/");
+    }
+    return res.status(418).redirect("/auth/signin");
+  }
+  return res.status(418).redirect("/auth/signin");
 };
 
 const authSignout = async (req, res) => {
-	req.session.destroy((err) => {
-		if (err) return res.redirect("/");
+  req.session.destroy((err) => {
+    if (err) return res.redirect("/");
 
-		res.clearCookie(req.app.get("cookieName"));
-		return res.redirect("/");
-	});
+    res.clearCookie(req.app.get("cookieName"));
+    return res.redirect("/");
+  });
 };
 
 module.exports = {
-	authSignup,
-	authSignin,
-	authSignout,
-	authSigninRender,
-	authSignupRender,
+  authSignup,
+  authSignin,
+  authSignout,
+  authSigninRender,
+  authSignupRender,
 };
--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-	name: { type: String, required: true },
-	email: { type: String, required: true },
-	pass: { type: String, required: true },
-	isAdmin: { type: Boolean, default: false },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  pass: { type: String, required: true },
+  passFass: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false },
 });
 
 const User = mongoose.model("User", userSchema);
